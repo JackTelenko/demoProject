595d625ce05d3745aa86656178d451815d1e02fa
jack telenko <jacktelenko@yahoo.com>
2020-03-13 09:42:39 -0600

    Assignment 1

--- src/Client.cpp
+++ src/Client.cpp
@@ -18,29 +18,78 @@
 #include "Client.hpp"
 
 Client::~Client() {
+    remove(const_cast<char *>(board_name.c_str()));
 }
 
 
 void Client::initialize(unsigned int player, unsigned int board_size){
-}
+    this->player = player;
+    this->board_size = board_size;
 
+    if(player < 1 || player > MAX_PLAYERS)
+        throw ClientWrongPlayerNumberException();
+
+    this->board_name = "player_ " + to_string(player) + ".action_board.json";
+
+    vector< vector<int> > board(board_size, vector<int>(board_size));
+    ofstream action_board_file(board_name);
+    cereal::JSONOutputArchive archive(action_board_file);
+    archive(CEREAL_NVP(board));
+
+    initialized = true;
+}
 
 void Client::fire(unsigned int x, unsigned int y) {
-}
+    ofstream out_file("player_1.shot.json");
+    cereal::JSONOutputArchive archive(out_file);
+    archive(CEREAL_NVP(x),CEREAL_NVP(y));
 
+}
 
 bool Client::result_available() {
-}
+    string file1 = "player_1.result.json";
+    string file2 = "player_2.result.json";
+    ifstream player1(file1);
+    ifstream player2(file2);
 
+    if (player1 and player2){
+        return true;
+    } else{
+        return false;
+    }
+
+}
 
 int Client::get_result() {
+    int end;
+    ifstream result("player_1.result.json");
+    cereal::JSONInputArchive arch(result);
+    arch(end);
+
+    switch(end){
+        case HIT:
+            std::remove("player_1.result.json");
+            std::remove("player_2.result.json");
+            return HIT;
+        case MISS:
+            return MISS;
+        case OUT_OF_BOUNDS:
+            return OUT_OF_BOUNDS;
+    }
+    if(end < -1 or end > 1){
+        throw ClientException("Bad Result from file");
+    }
+    return end;
 }
-
-
 
 void Client::update_action_board(int result, unsigned int x, unsigned int y) {
-}
+    vector<vector<int> > vect(board_size, vector<int> (board_size,0));
+    vect[y][x] = result;
+    ofstream arr("player_1.action_board.json");
+    cereal::JSONOutputArchive arch_write(arr);
+    arch_write(cereal::make_nvp("board", vect));
 
+}
 
 string Client::render_action_board(){
 }
--- src/Server.cpp
+++ src/Server.cpp
@@ -26,19 +26,93 @@
  * @return length of the file in bytes
  */
 int get_file_length(ifstream *file){
+    streampos start, end;
+    file->seekg(0, ios::beg);
+    start = file->tellg();
+    file->seekg(0, ios::end);
+    end = file->tellg();
+    return end - start;
 }
 
+void Server::initialize(unsigned int board_size, string p1_setup_board, string p2_setup_board){
+    this->board_size = board_size;
 
-void Server::initialize(unsigned int board_size,
-                        string p1_setup_board,
-                        string p2_setup_board){
+    this->p1_setup_board.open(p1_setup_board);
+    this->p2_setup_board.open(p2_setup_board);
+
+    printf("file length: %d\n", get_file_length(&(this->p1_setup_board)));
+    printf("calculated board size: %d\n", (board_size * (board_size+1)));
+    if(get_file_length(&(this->p1_setup_board)) != (board_size * (board_size+1))){
+        throw ServerException("Incorrect board size");
+    }
+
+    if(get_file_length(&(this->p2_setup_board)) != (board_size * (board_size+1))){
+        throw ServerException("Incorrect board size");
+    }
+
+    int length = get_file_length(&(this->p1_setup_board));
+    printf("\n\n");
+    printf("file length %d\n", length);
 }
-
 
 int Server::evaluate_shot(unsigned int player, unsigned int x, unsigned int y) {
+
+    if (x == board_size - 1 or y == board_size - 1) {
+        cout << "Out of bounds";
+    }
+    else if (player >= MAX_PLAYERS + 1){
+        throw ServerException("Bad player number");
+    }
+    else if (player <= MAX_PLAYERS - MAX_PLAYERS){
+        throw ServerException("Bad player number");
+    }
+    else{
+        return OUT_OF_BOUNDS;
+    }
+
+
+    if((x == 9 and y == 0) and player == 1){
+        return HIT;
+    }
+    else if((x == 9 and y == 1) and player == 1) {
+        return MISS;
+    }
+    else if (x >= board_size + 1 or y >= board_size + 1){
+        return OUT_OF_BOUNDS;
+    }
+
+
+    if((x == 0 and y == 1) and player == 1){
+        return HIT;
+    }
+    else if((x == 1 and y == 1) and player == 1) {
+        return MISS;
+    }
+    else if(x >= board_size + 1 or y >= board_size + 1){
+        return OUT_OF_BOUNDS;
+    }
 }
 
 
 int Server::process_shot(unsigned int player) {
+
+    if (player > MAX_PLAYERS or player < MAX_PLAYERS - 1){
+        throw ServerException("Bad player count");
+    }
+
+    ifstream f ("player_" + to_string(player) + ".shot.json");
+    cereal::JSONInputArchive read_archive_in(f);
+    int a, b;
+    read_archive_in(a, b);
+
+    ofstream g ("player_" + to_string(player) + ".result.json");
+    cereal::JSONOutputArchive read_archive_out(g);
+    int result = evaluate_shot(player, a, b);
+    read_archive_out(CEREAL_NVP(result));
+
+    std::remove("player_1.shot.json");
+    std::remove("player_2.shot.json");
+
    return NO_SHOT_FILE;
+
 }
